7a4b3b2c3028c6949941910a26070636f31e2886
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-05-03 20:20:30 -0300

    1 file modified

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -8,7 +8,7 @@ import { TaskService } from '../task.service';
 })
 export class TaskComponent implements OnInit {
 
-  constructor(public subjects: TaskService) { }
+  constructor(public task: TaskService) { }
 
   ngOnInit() {
   }
