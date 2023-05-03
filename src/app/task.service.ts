3a6a64ebf270ed38f824e54a7da6d237d2808093
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-05-03 20:14:59 -0300

    2 files modified

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -2,14 +2,33 @@ import { Injectable } from '@angular/core';
 
 interface Tasks {
   title: String;
+  day: string;
+  hour: string;
 }
 
 @Injectable()
 export class TaskService {
   list: Array<Tasks> = [
-    {title: 'Banco de Dados II'},
-    {title: 'Desenvolvimento para Servidores II'},
-    {title: 'Tópicos Especiais II'},
+    {
+      title: "Projeto de Encontrabilidade",
+      day: "Segunda-feira",
+      hour: "19h - 22h30",
+    },
+    {
+      title: "Banco de Dados II",
+      day: "Quarta-feira",
+      hour: "9h30 - 13h",
+    },
+    {
+      title: "Tópicos Especiais II",
+      day: "Sexta-feira",
+      hour: "19h - 22h30",
+    },
+    {
+      title: "Desenvolvimento para Servidores II",
+      day: "Quinta-feira",
+      hour: "19h - 22h30",
+    },
   ];
 
   constructor() {}
@@ -18,8 +37,8 @@ export class TaskService {
     return this.list;
   }
 
-  add(title: string) {
-    this.list.push({title});
+  add(title: string, day: string, hour:string, ) {
+    this.list.push({ title, day, hour});
   }
 
   remove(index: number) {
